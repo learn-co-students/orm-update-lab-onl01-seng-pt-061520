b36ec91474c19c7cea4fb90c33be990e45e707bb
drewsview34 <drewsview34@outlook.com>
2020-08-26 19:58:03 -0700

    Created update instance method

--- lib/student.rb
+++ lib/student.rb
@@ -54,5 +54,14 @@ class Student
     end
   end
 
+  def update
+    sql = <<-SQL
+      UPDATE students
+      SET name = ?, grade = ?
+      WHERE id = ?
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 
 end
